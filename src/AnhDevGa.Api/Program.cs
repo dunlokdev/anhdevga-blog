21c3722c898dd56fb5796a3b3551b62f0614cf69
DunlokDev <myloc442@gmail.com>
2024-06-06 22:18:10 +0700

    Config Identity & DbContext

--- src/AnhDevGa.Api/Program.cs
+++ src/AnhDevGa.Api/Program.cs
@@ -1,6 +1,41 @@
+using AnhDevGa.Core.Domain.Identity;
+using AnhDevGa.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
+
+// Config DB context and ASP.NET CORE Identity
+builder.Services.AddDbContext<AnhDevGaContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<AnhDevGaContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+    options.User.RequireUniqueEmail = false;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/AnhDevGa.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnhDevGa.Core.Domain.Identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName { get; set; }
+    }
+}
